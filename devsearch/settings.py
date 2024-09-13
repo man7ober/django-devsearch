f45d048877361a0271a301126a59c5591950e704
man7ober <man7ober@gmail.com>
2024-09-13 09:43:56 +0530

    hide the secrets

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -107,8 +107,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'man7ober@gmail.com'
-EMAIL_HOST_PASSWORD = 'bnicxukrcvpeggya'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # Static files (CSS, JavaScript, Images)
 
